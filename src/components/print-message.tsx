d86bc72e0057e50ed7c06848a2f805496418ad70
sumeyacodes <sumeya4hmed@gmail.com>
2025-07-14 18:52:00 +0100

    fix: print message url & print bg

--- src/components/print-message.tsx
+++ src/components/print-message.tsx
@@ -1,7 +1,13 @@
 export const PrintMessage = () => (
   <span className="print hidden print:block text-secondary/80 text-center text-sm italic">
     Printable copy, please see{" "}
-    <span className="text-accent font-semibold">https://sumeya.dev</span> for
-    live web version
+    <a
+      href="https://sumeya.dev/"
+      className="text-accent font-semibold"
+      target="_blank"
+      rel="noopener noreferrer">
+      sumeya.dev
+    </a>{" "}
+    for live web version
   </span>
 );
